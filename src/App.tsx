6ba1c6c016488842774da2ae60094d9fb7e3cc33
warner <warner@mail.uni-paderborn.de>
2024-04-26 09:38:28 +0200

    added feature buttons

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Button } from '@mui/material';
 
 function App() {
   return (
     <div>
-      <h1>Trööt</h1>
+      <h1>Mix and Magic</h1>
+        <Button variant='contained'>Cake Configurator</Button>
+        <Button variant='contained'>Personal Projects</Button>
+        <Button variant='contained'>Flavor Finder</Button>
+        <Button variant='contained'>Refined Recipes</Button>
     </div>
   );
 }
