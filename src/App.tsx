775714fa22688273f878f8af0440d27d4c77dc86
warner <warner@mail.uni-paderborn.de>
2024-04-26 10:59:52 +0200

    added basic routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,15 @@
 import React from 'react';
 import './App.css';
-import { Button } from '@mui/material';
+import { Route, Routes } from 'react-router-dom';
+import Home from './view/pages/home';
+import CakeConfigurator from './view/pages/cake-configurator';
 
 function App() {
   return (
-    <div>
-      <h1>Mix and Magic</h1>
-        <Button variant='contained'>Cake Configurator</Button>
-        <Button variant='contained'>Personal Projects</Button>
-        <Button variant='contained'>Flavor Finder</Button>
-        <Button variant='contained'>Refined Recipes</Button>
-    </div>
+    <Routes>
+      <Route path='/' element={<Home/>}/>
+      <Route path='cake-configurator' element={<CakeConfigurator/>}/>
+    </Routes>
   );
 }
 
