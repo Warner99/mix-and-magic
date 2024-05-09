794e45fe824b1d764b217e74495e561c4e725541
warner <warner@mail.uni-paderborn.de>
2024-05-09 23:16:15 +0200

    added component structure

--- src/view/pages/refined-recipes.tsx
+++ src/view/pages/refined-recipes.tsx
@@ -1,4 +1,11 @@
 export function RefinedRecipes(){
-    return(<div> <h1>Refined Recipes</h1></div>);
+    return(
+    <div> 
+        <h1>Refined Recipes</h1>
+        <h2>Search Bar - Autocomplete</h2>
+        <h2>Filter</h2>
+        <h2>Recipe Overview</h2>
+    </div>
+    );
 }
 export default RefinedRecipes;
