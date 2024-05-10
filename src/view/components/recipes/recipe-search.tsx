9d8428dfb00890e8709e88275f8fb432d07ca2e4
warner <warner@mail.uni-paderborn.de>
2024-05-10 23:56:04 +0200

    added search bar component

---
+++ src/view/components/recipes/recipe-search.tsx
@@ -0,0 +1,16 @@
+import { Autocomplete, AutocompleteRenderInputParams } from "@mui/material";
+import { ReactNode } from "react";
+
+export function RecipeSearch(){
+    return(
+    <div>
+        {/* <Autocomplete 
+            renderInput={function (params: AutocompleteRenderInputParams): ReactNode {
+                throw new Error("Function not implemented.");
+            } } options={[]}>
+            
+        </Autocomplete> */}
+        Search Bar
+    </div>);
+}
+export default RecipeSearch;
--- src/view/pages/refined-recipes.tsx
+++ src/view/pages/refined-recipes.tsx
@@ -1,8 +1,11 @@
+import RecipeSearch from "../components/recipes/recipe-search";
+
 export function RefinedRecipes(){
     return(
     <div> 
         <h1>Refined Recipes</h1>
         <h2>Search Bar - Autocomplete</h2>
+        <RecipeSearch/>
         <h2>Filter</h2>
         <h2>Recipe Overview</h2>
     </div>
